f05e0ad53a7678700be9531ef058cd1fd04270fe
caseypugh <me@caseypugh.com>
2011-03-28 15:24:42 -0400

    mc updates

--- logger.php
+++ logger.php
@@ -1,5 +1,4 @@
 <?
-date_default_timezone_set('America/New_York');
 
 $ch = curl_init("http://dl.dropbox.com/u/19353/Minecraft/server.log");
 curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -129,52 +128,3 @@ foreach ($logs as $l)
   else if (preg_match("/Stopping server/i", $l, $m))
     server_quit();
 }
-
-$users = getusers();
-
-?>
-<html>
-  <head>
-    <title>Casey's Minecraft Server (cpu.mypets.ws)</title>
-    <link rel="stylesheet" type="text/css" href="/assets/css/reset.css" media="screen" />
-    <style>
-
-      .server { font: bold 24px Helvetica, Arial; padding: 20px 10px; color: #7ed471; background: #333; border-bottom: 1px solid #fff; }
-      .users .user { padding: 10px; clear: both; border-bottom: 1px solid #ddd; }
-      .users .user img { float: left; }
-      .users .user .info { margin-top: 15px; float: left; margin-left: 10px; }
-      .users .user .info h1 { font: 46px Helvetica, Arial; color: #333; }
-      .users .user .info span { color: #999; font: normal 14px Helvetica, Arial; }
-      .users .user .info span.on { color: #7ed471; }
-
-
-      .users .offline .info h1 { color: #666; }
-      .users .offline { background: #eee;  }
-      .clear { clear: both; }
-
-    </style>
-  </head>
-  <body>
-    <div class="server">
-      cpu.mypets.ws
-    </div>
-    <div class="users">
-
-      <? foreach ($users as $u): ?>
-        <div class="user <?= $u['online'] ? 'online' : 'offline' ?>">
-          <img src="<?= $u['avatar'] ?>" />
-          <div class="info">
-            <h1><?= $u['name'] ?></h1>
-            <? if (!$u['online']): ?>
-              <span>Offline. Last seen <?= getTimeAgo($u['time']) ?></span>
-            <? else: ?>
-              <span class="on">Online!</span>
-            <? endif ?>
-          </div>
-          <div class="clear"></div>
-        </div>
-      <? endforeach ?>
-
-    </div>
-  </body>
-</html>
