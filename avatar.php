591aff179e49e6f1a3138d469c085b47ede1b448
caseypugh <me@caseypugh.com>
2012-04-18 15:13:30 -0400

    add the helmet code back in

--- avatar.php
+++ avatar.php
@@ -61,6 +61,28 @@ if(!$src){
 $dest   = imagecreatetruecolor(8, 8);
 imagecopy($dest, $src, 0, 0, 8, 8, 8, 8);   // copy the face
 
+// Check to see if the helm is not all same color
+$bg_color = imagecolorat($src, 0, 0);
+$no_helm  = true;
+
+// Check if there's any helm
+for ($i = 1; $i <= 8; $i++) {
+  for ($j = 1; $j <= 4; $j++) {
+    // scanning helm area
+    if (imagecolorat($src, 40 + $i, 7 + $j) != $bg_color) {
+      $no_helm = false;
+    }
+  }
+
+  if (!$no_helm)
+    break;
+}
+
+// copy the helm
+if (!$no_helm) {
+  imagecopy($dest, $src, 0, -1, 40, 7, 8, 4);
+}
+
 //prepare to finish the image
 $final = imagecreatetruecolor($size, $size);
 imagecopyresized($final, $dest, 0, 0, 0, 0, $size, $size, 8, 8);
